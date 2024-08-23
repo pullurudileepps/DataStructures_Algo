008f7056f8f6019536c754caf683443b340f5b37
P DILEEP <157283596+pullurudileepps@users.noreply.github.com>
2024-08-23 13:25:43 +0530

    Update GCD.java

--- src/main/java/BasicMath/GCD.java
+++ src/main/java/BasicMath/GCD.java
@@ -10,10 +10,10 @@ public class GCD {
     private static int EuclideanAlgo(int n1, int n2) {
         while (n1 != 0 && n2 != 0) {
             if (n1 > n2) {
-                //n1 = n1 - n2;//it is take more iterations
+                //n1 = n1 - n2;//it takes more iterations
                 n1 = n1 % n2;
             } else {
-                //n2 = n2 - n1;//it is take more iterations
+                //n2 = n2 - n1;//it takes more iterations
                 n2 = n2 % n1;
             }
         }
